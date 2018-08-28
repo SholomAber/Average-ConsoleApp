873e7a79b0862bde31529300377d5b35c0656aae
Sholom Aber <SholomAber@users.noreply.github.com>
2018-08-27 22:13:27 -0400

    initial commit

---
+++ AverageConsoleApp/ClassAverage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class ClassAverage
+{
+    static void Main()
+    {
+        int total = 0;
+        int grades = 1;
+        int studentCount = 0;
+
+        Console.Write("Please enter the amount of students in your class: ");
+        studentCount = int.Parse(Console.ReadLine());
+
+        while (grades <= studentCount)
+        {
+            Console.Write("Please enter the grade: ");
+            total += int.Parse(Console.ReadLine());
+            grades++;
+        }
+
+        Console.WriteLine($"The class total is {total}. The class average is {total/studentCount}.");
+    }
+}
