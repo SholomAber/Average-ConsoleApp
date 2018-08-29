139c25dfa53a6474635f4397d1dfc8afe589b4b0
Sholom Aber <SholomAber@users.noreply.github.com>
2018-08-28 20:11:26 -0400

    updated variable names

--- AverageConsoleApp/ClassAverage.cs
+++ AverageConsoleApp/ClassAverage.cs
@@ -9,20 +9,20 @@ class ClassAverage
     static void Main()
     {
         int total = 0;
-        int grades = 1;
-        int studentCount = 0;
+        int studentCounter = 1;
+        int studentTotal = 0;
 
         Console.Write("Please enter the amount of students in your class: ");
-        studentCount = int.Parse(Console.ReadLine());
+        studentTotal = int.Parse(Console.ReadLine());
 
-        while (grades <= studentCount)
+        while (studentCounter <= studentTotal)
         {
-            Console.Write($"Please enter the grade for student {grades}: ");
+            Console.Write($"Please enter the grade for student {studentCounter}: ");
             total += int.Parse(Console.ReadLine());
-            grades++;
+            studentCounter++;
         }
 
-        Console.WriteLine($"The class total is {total}. The class average is {total/studentCount}.");
+        Console.WriteLine($"The class total is {total}. The class average is {total/studentTotal}.");
 
         Console.ReadKey();
     }
